82967c1ef030ca4621e23544a57050aadb52c8d4
Peterka <peterkaa22@spstrutnov.cz>
2023-02-11 16:29:44 +0100

    Background

--- Hra.py
+++ Hra.py
@@ -1,27 +1,41 @@
-import sys
-import random
 import pygame
+import math
 
 pygame.init()
 
-ROZLISENI_X = 1920
-ROZLISENI_Y = 1018
+clock = pygame.time.Clock()
 FPS = 60
-CERNA_BARVA = (0, 0, 0)
-BILA_BARVA = (255, 255, 255)
 
-velikost = 50
-pozice_x = (ROZLISENI_X - velikost) / 2
-pozice_y = (ROZLISENI_Y - velikost) / 2
-rychlost = 3
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 600
 
-okno = pygame.display.set_mode((ROZLISENI_X, ROZLISENI_Y))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("dinosaurus")
 
-while True:
-     for udalost in pygame.event.get():
-          if udalost.type == pygame.QUIT:
-               pygame.quit()
-               sys.exit()
+bg = pygame.image.load("bg.png").convert()
+bg_width = bg.get_width()
+
+scroll = 0
+tiles = math.ceil(SCREEN_WIDTH  / bg_width) + 1
+
+run = True
+while run:
+
+  clock.tick(FPS)
+
+  for i in range(0, tiles):
+    screen.blit(bg, (i * bg_width + scroll, 0))
     
+  scroll -= 5
+
+  if abs(scroll) > bg_width:
+    scroll = 0
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+  pygame.display.update()
+
+pygame.quit()
     
