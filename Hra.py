92e7b4af02d13ee6cb0fb10c76cc40b3dd1a9851
Peterka <peterkaa22@spstrutnov.cz>
2023-03-27 21:25:27 +0200

    restart

--- Hra.py
+++ Hra.py
@@ -24,7 +24,7 @@ hrac_start_zivot = 3
 bila = (255, 255, 255)
 probiha_kolize = False
 konec_kolize = False 
-
+konec_hry = False 
 
 hrac_zivot = hrac_start_zivot
 
@@ -33,6 +33,8 @@ pygame.display.set_caption("dinosaurus")
 
 zivot_font = pygame.font.SysFont("arialblack", 20)
 font = pygame.font.SysFont("arialblack", 20)
+konec_font = pygame.font.SysFont("arialblack", 40)
+restart_font = pygame.font.SysFont("arialblack", 20)
 
 bg = pygame.image.load("bg.png")
 bg_rect = bg.get_rect()
@@ -68,120 +70,144 @@ while True:
             sys.exit()
         elif udalost.type == timer_event:
             time_left +=1
+    
+    if hrac_zivot < 1:
+            konec_hry = True
+               
+    
+    
+    if konec_hry == False:
+                
+        zivot_text = zivot_font.render(f"Životy: {hrac_zivot}", True, bila)
+        zivot_text_rect = zivot_text.get_rect()
+        zivot_text_rect.right = w - 20
+        zivot_text_rect.top = 15
+
+        timer_text = font.render(f"Čas:{time_left}", True, (255,255,255))
+        
+        konec_text = konec_font.render("Zemřel jsi!" , True, (255, 0, 0))                         
+        
+        restart_text = restart_font.render("Zmáčkni CTRL pro Restart" ,True, (255, 255, 255))
+        
+        screen.blit(bg, bg_rect)
+        screen.blit(hero,(100,vyška))
+        screen.blit(krecek_enemy, krecek_rect)
+        screen.blit(zivot_text, zivot_text_rect)
+        screen.blit(timer_text, (30,15))
+        screen.blit(srdce, srdce_rect)
+        
+        pygame.draw.line(screen, bila, (0,60), (w, 60), 2)
+        
+        
+        klavesa = pygame.key.get_pressed()
+        if klavesa[pygame.K_SPACE] and not (jump or padani):
+            jump = True
+        
+        if jump:
+            vyška = vyška-5
+
+        if vyška < v_konec:
+            jump = False
+            padani = True 
+        
+        if padani:
+            vyška = vyška+5
             
-    zivot_text = zivot_font.render(f"Životy: {hrac_zivot}", True, bila)
-    zivot_text_rect = zivot_text.get_rect()
-    zivot_text_rect.right = w - 20
-    zivot_text_rect.top = 15
+            
+        if v_dolu < vyška: 
+            padani = False 
 
-    timer_text = font.render(f"Čas:{time_left}", True, (255,255,255))
-                             
-    
-    screen.blit(bg, bg_rect)
-    screen.blit(hero,(100,vyška))
-    screen.blit(krecek_enemy, krecek_rect)
-    screen.blit(zivot_text, zivot_text_rect)
-    screen.blit(timer_text, (30,15))
-    screen.blit(srdce, srdce_rect)
-    
-    pygame.draw.line(screen, bila, (0,60), (w, 60), 2)
-    
-    klavesa = pygame.key.get_pressed()
-    if klavesa[pygame.K_SPACE] and not (jump or padani):
-        jump = True
-    
-    if jump:
-        vyška = vyška-5
+        if krecek_rect.x < 0:
+            krecek_rect.x = w + krecek_behind_border
+            krecek_rect.y = (568 - 40)
+            probiha_kolize = False
+        else:
+            krecek_rect.x -= krecek_rychlost
+        
+        if krecek_rect.x < 100 + 80 and not probiha_kolize and vyška > 470:
+            probiha_kolize = True
+            hrac_zivot -= 1
+            
+                          
+     
+        if srdce_rect.x < 0:
+            srdce_rect.x = w + srdce_behind_border
+            srdce_rect.y = (556)
+            probiha_kolize = False
+        else:
+            srdce_rect.x -= srdce_rychlost
+                       
+        if srdce_rect.x < 180 and not probiha_kolize and vyška > 470:
+            probiha_kolize = True
+            hrac_zivot += 1
+        
+        if time_left > 10:
+            krecek_rychlost = 7
+            
+        if time_left > 20:
+            krecek_rychlost = 8
+        
+        if time_left > 30:
+            krecek_rychlost = 9
+            
+        if time_left > 40:
+            krecek_rychlost = 10
+        
+        if time_left > 50:
+            krecek_rychlost = 10.5
+            
+        if time_left > 60:
+            krecek_rychlost = 11
+            
+        if time_left > 70:
+            krecek_rychlost = 11.5
+            
+        if time_left > 80:
+            krecek_rychlost = 12
+       
+        if time_left > 70:
+            krecek_rychlost = 12.5
 
-    if vyška < v_konec:
-        jump = False
-        padani = True 
-    
-    if padani:
-        vyška = vyška+5
+        if time_left > 80:
+            krecek_rychlost = 13
+            
+        if time_left > 90:
+            krecek_rychlost = 13.5
+            
+        if time_left > 100:
+            krecek_rychlost = 14
+            
+        if time_left > 110:
+            krecek_rychlost = 14.5
+            
+        if time_left > 120:
+            krecek_rychlost = 15
+            
+        if time_left > 130:
+            krecek_rychlost = 15.5
         
-        
-    if v_dolu < vyška: 
-        padani = False 
-
-    if krecek_rect.x < 0:
-        krecek_rect.x = w + krecek_behind_border
-        krecek_rect.y = (568 - 40)
-        probiha_kolize = False
-    else:
-        krecek_rect.x -= krecek_rychlost
-    
-    if krecek_rect.x < 100 + 80 and not probiha_kolize and vyška > 470:
-        probiha_kolize = True
-        hrac_zivot -= 1
-        
-                      
- 
-    if srdce_rect.x < 0:
-        srdce_rect.x = w + srdce_behind_border
-        srdce_rect.y = (556)
-        probiha_kolize = False
-    else:
-        srdce_rect.x -= srdce_rychlost
-                   
-    if srdce_rect.x < 180 and not probiha_kolize and vyška > 470:
-        probiha_kolize = True
-        hrac_zivot += 1
-    
-    if time_left > 10:
-        krecek_rychlost = 7
-        
-    if time_left > 20:
-        krecek_rychlost = 8
-    
-    if time_left > 30:
-        krecek_rychlost = 9
-        
-    if time_left > 40:
-        krecek_rychlost = 10
-    
-    if time_left > 50:
-        krecek_rychlost = 10.5
-        
-    if time_left > 60:
-        krecek_rychlost = 11
-        
-    if time_left > 70:
-        krecek_rychlost = 11.5
-        
-    if time_left > 80:
-        krecek_rychlost = 12
+        if time_left > 140:
+            krecek_rychlost = 16
+            
+        if time_left > 150:
+            krecek_rychlost = 16.5
+            
+        if time_left > 160:
+            krecek_rychlost = 17
    
-    if time_left > 70:
-        krecek_rychlost = 12.5
-
-    if time_left > 80:
-        krecek_rychlost = 13
+        if hrac_zivot < 1:
+            screen.blit(konec_text, (315,300))
+            screen.blit(restart_text, (285, 360))
         
-    if time_left > 90:
-        krecek_rychlost = 13.5
+                
+            
+            
         
-    if time_left > 100:
-        krecek_rychlost = 14
-        
-    if time_left > 110:
-        krecek_rychlost = 14.5
-        
-    if time_left > 120:
-        krecek_rychlost = 15
-        
-    if time_left > 130:
-        krecek_rychlost = 15.5
+            
+                
     
-    if time_left > 140:
-        krecek_rychlost = 16
-        
-    if time_left > 150:
-        krecek_rychlost = 16.5
-        
-    if time_left > 160:
-        krecek_rychlost = 17
-   
+            
+            
     pygame.display.update()
 
     clock.tick(FPS)
